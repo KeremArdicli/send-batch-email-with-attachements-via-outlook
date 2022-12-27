83df39203568b19eb9af634510f5510a1e0e7a14
KeremArdicli <72523365+KeremArdicli@users.noreply.github.com>
2022-12-27 15:27:26 +0300

    add try except block and reporting

--- mail.py
+++ mail.py
@@ -2,27 +2,35 @@ import csv
 from time import sleep
 import win32com.client as client
 
-with open('mailbody.html', 'r', encoding='utf-8') as f:     # prepare an html file named mailbody.htm and but it in the same directory.
-    html_string = f.read() 
+with open('mailbody.html', 'r', encoding='utf-8') as f:
+    html_string = f.read()
     
-template = html_string 
-# template = "{} , Mail body here."  ---------------- to use plain text activate this line and deactivate the line above.
+template = html_string
 
-with open('people.csv', 'r', newline='') as f:
+
+with open('people2.csv', 'r', newline='') as f:
      reader = csv.reader(f)
      distro = [row for row in reader]
 
+f= open("report.txt","w+")
 
-chunks = [distro[x:x+4] for x in range (0, len(distro), 4)]   #change the numbers in this line according to the number of rows in your csv file. for eg: if you have 20 people, change 4s to 20.
+chunks = [distro[x:x+4] for x in range (0, len(distro), 4)]  # 2 people per chunk
 outlook = client.Dispatch('Outlook.Application')
 
 for chunk in chunks:
     for name, email, attach in chunk:
-        message = outlook.CreateItem(0)
-        message.To = email
-        message.Subject = "Mail Subject Here"
-        message.Body = template.format(name) # for html templates, change this line to "message.HTMLBody = template"
-        attachement = attach
-        message.Attachments.Add(attachement)
-        message.Send()
-    sleep(60)
+        try:
+            sign = "someimage.png"  #url of your mail signature 
+            message = outlook.CreateItem(0)
+            message.To = email
+            message.Cc  = 'example@example.com'
+            message.Subject = "HTML dosyadan çekme denemesi"
+            message.HTMLBody = template.format(name=name, sign=sign)  # change line to message.Body if you like to send plain text
+            attachement = attach
+            message.Attachments.Add(attachement)
+            message.Send()
+        except:
+            print("Mail could not sent to: ", name)
+            f.write("Mail could not sent to: " + name + "\n")
+    sleep(3)
+f.close()
